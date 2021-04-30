e185580312f9ad136afa542924df502e9376ee98
czechbol <adamludes@gmail.com>
2021-04-30 11:56:01 +0200

    version bump for PyPI

--- mathcrypto/__init__.py
+++ mathcrypto/__init__.py
@@ -2,4 +2,4 @@ from .funcs import MathFunctions  # noqa: F401
 from .groups import MultiplicativeGroup  # noqa: F401
 from .crypto import DHCryptosystem, DHCracker, Primes  # noqa: F401
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
