7a364473fc3b1d8c890beadbeddabb826a165b76
czechbol <adamludes@gmail.com>
2021-10-03 15:38:58 +0200

    testing prime generation

--- tests/test_cryptography_primes.py
+++ tests/test_cryptography_primes.py
@@ -3,6 +3,11 @@ import pytest
 from mathcrypto.cryptography.primes import Primes
 
 
+@pytest.mark.parametrize("num", [32, 64, 128])
+def test_get_prime(num):
+    assert Primes.is_probable_prime_fermat(Primes.get_prime(num), 10)
+
+
 @pytest.mark.parametrize("num,expected", [(13, True), (240, False), (17, True)])
 def test_is_prime(num, expected):
     assert Primes.is_prime(num) == expected
